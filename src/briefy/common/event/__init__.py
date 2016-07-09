a02ae9f220b81d9db9af3b61c641b074ae05e575
Érico Andrei <ericof@gmail.com>
2016-07-09 11:22:20 -0300

    Add base event

---
+++ src/briefy/common/event/__init__.py
@@ -0,0 +1,70 @@
+"""Briefy base events."""
+from briefy.common.queue import IQueue
+from zope.component import getUtility
+from zope.interface import Interface
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class IEvent(Interface):
+    """Interface for Events on Briefy."""
+
+
+class IDataEvent(IEvent):
+    """Interface for data events on Briefy."""
+
+
+class BaseEvent:
+    """A base event class for briefy."""
+
+    event_name = ''
+    actor = ''
+    guid = ''
+    created_at = ''
+    request_id = ''
+    logger = logger
+    data = None
+
+    def __init__(self, obj, actor=None, request_id=None):
+        """Initialize the event."""
+        if not obj.created_at:
+            raise ValueError('Attempt to create event without a timestamp. Has it been persisted?')
+        guid = obj.id
+        self.actor = actor
+        self.guid = guid
+        self.request_id = request_id
+        self.created_at = obj.created_at.isoformat()
+        self.data = obj.to_JSON()
+
+    @property
+    def queue(self):
+        """Return the events queue."""
+        return getUtility(IQueue, 'events.queue')
+
+    def __call__(self):
+        """Notify about the event.
+
+        :returns: Id from message in the queue
+        :rtype: str
+        """
+        logger = self.logger
+        queue = self.queue
+        payload = {
+            'event_name': self.event_name,
+            'actor': self.actor,
+            'guid': self.guid,
+            'created_at': self.created_at,
+            'request_id': self.request_id,
+            'data': self.data,
+        }
+        message_id = ''
+        try:
+            message_id = queue.write_message(payload)
+        except:
+            logger.error('Event {} not fired'.format(self.event_name), extra={'payload': payload})
+        else:
+            logger.debug('Event {} fired with message {}'.format(self.event_name, message_id))
+
+        return message_id
