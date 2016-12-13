c0264121c777e275c82012e66adffadeddd12d9b
Érico Andrei <ericof@gmail.com>
2016-12-13 12:57:17 +0100

    Slug is now indexed and will have a generated value

--- src/briefy/common/db/mixins/metadata.py
+++ src/briefy/common/db/mixins/metadata.py
@@ -1,5 +1,6 @@
 """Base metadata mixin."""
-from briefy.common.utils.data import generate_slug
+from briefy.common.utils.data import generate_contextual_slug
+from sqlalchemy.ext.hybrid import hybrid_property
 
 import colander
 import sqlalchemy as sa
@@ -39,6 +40,7 @@ class BaseMetadata:
     _slug = sa.Column('slug',
                       sa.String(255),
                       nullable=True,
+                      index=True,
                       info={'colanderalchemy': {
                           'title': 'Description',
                           'missing': colander.drop,
@@ -49,34 +51,26 @@ class BaseMetadata:
     To be used in url.
     """
 
-    @property
+    @hybrid_property
     def slug(self) -> str:
         """Return a slug for an object.
 
-        If slug has not been set on this object, fallback to a composition of
-        the first 8 chars of an id field, and a slug of the title itself.
-
         :return: A slug to be added to an url.
         """
-        slug = self._slug
-        if not slug:
-            obj_id = getattr(self, 'id', None)
-            if obj_id:
-                # Return just the first 8 chars of an uuid
-                obj_id = str(obj_id)[:8]
-            else:
-                obj_id = ''
-            title = str(self.title)
-            slug = '{obj_id}-{title}'.format(
-                obj_id=obj_id,
-                title=generate_slug(title)
-            )
-        return slug
+        return self._slug
 
     @slug.setter
     def slug(self, value: str):
         """Set a new slug for this object.
 
+        If the value is None, we generate a new one using
+        :func:`briefy.common.utils.data.generate_contextual_slug`
         :param value: Value of the new slug
         """
+        if not value:
+            context = {
+                'id': getattr(self, 'id', ''),
+                'title': getattr(self, 'title', ''),
+            }
+            value = generate_contextual_slug(context)
         self._slug = value
--- src/briefy/common/utils/data.py
+++ src/briefy/common/utils/data.py
@@ -18,3 +18,23 @@ def generate_slug(value: str) -> str:
     :return: A url-friendly slug of a value.
     """
     return slugify(value)
+
+
+def generate_contextual_slug(context: dict) -> str:
+    """Given a context, calculate the slug for it.
+
+    :param context: Dictionary containing some values like id and title.
+    :return: A url-friendly slug of a value.
+    """
+    obj_id = context.get('id', '')
+    if obj_id:
+        # Return just the first 8 chars of an uuid
+        obj_id = str(obj_id)[:8]
+    else:
+        obj_id = ''
+    title = context.get('title', '')
+    slug = '{obj_id}-{title}'.format(
+        obj_id=obj_id,
+        title=generate_slug(title)
+    )
+    return slug
