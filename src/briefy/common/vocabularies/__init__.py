c3156708308cff3e24289270601e06581eb2e332
Joao S. O. Bueno <jsbueno@picsastock.com>
2017-05-04 12:35:35 -0300

    Fix LabeledEnum pickling issues

--- src/briefy/common/vocabularies/__init__.py
+++ src/briefy/common/vocabularies/__init__.py
@@ -1,7 +1,30 @@
 """Vocabularies used by Briefy systems."""
 from enum import Enum
+from importlib import import_module
 
 import sys
+
+
+# Due to being defined out of their native '__module__', these labeled enums
+# were not able to be pickled or unpickled (even with __module__ an __qualname__
+# being explicitly set).
+# This subclass and helper function override pickle's behavior for our Enums
+# so that they work.
+
+
+def _safe_pickle_load(module_name, class_name, item_name):
+    """Called by pickle.load with information to retrieve an Enum item."""
+    module = import_module(module_name)
+    enum_class = getattr(module, class_name)
+    return getattr(enum_class, item_name)
+
+
+class SafePickeableEnum(Enum):
+    """Safely pickleable version of Enum."""
+
+    def __reduce_ex__(self, protocol):
+        """Return the callable and parameters needed to unpickle an Enum item."""
+        return (_safe_pickle_load, (self.__module__, self.__class__.__name__, self.name))
 
 
 def LabeledEnum(class_name: str, names: list) -> Enum:
@@ -23,7 +46,7 @@ def LabeledEnum(class_name: str, names: list) -> Enum:
     else:
         enum_names = [(item[0], item[0]) for item in names]
         label_index = 1
-    new_enum = Enum(class_name, names=enum_names, module=module, qualname=qualname)
+    new_enum = SafePickeableEnum(class_name, names=enum_names, module=module, qualname=qualname)
     for enum_item, item in zip(new_enum, names):
         enum_item.label = item[label_index]
     return new_enum
--- tests/vocabularies/test_labeled_enums.py
+++ tests/vocabularies/test_labeled_enums.py
@@ -1,30 +1,43 @@
 """Tests for `briefy.common.vocabularies.roles` module."""
 from briefy.common.vocabularies import LabeledEnum
 
+import pytest
 
-def test_labeled_enums():
-    options = (
-
-        ('food', 'food', 'Food'),
-        ('interior', 'interior', 'Interior'),
-        ('journalism', 'journalism', 'Journalism/Documentary'),
-        ('landscape', 'landscape', 'Landscape'),
-
-    )
-    Categories = LabeledEnum('Categories', options)
-    assert str(Categories.interior) == 'Categories.interior'
-    assert Categories.interior.value == 'interior'
-    assert Categories.interior.label == 'Interior'
-
-
-def test_labeled_enums_with_2_items():
+@pytest.fixture
+def options():
     options = (
         ('food', 'Food'),
         ('interior', 'Interior'),
         ('journalism', 'Journalism/Documentary'),
         ('landscape', 'Landscape'),
     )
+    return options
+
+def test_labeled_enums():
+    options = (
+        ('food', 'food', 'Food'),
+        ('interior', 'interior', 'Interior'),
+        ('journalism', 'journalism', 'Journalism/Documentary'),
+        ('landscape', 'landscape', 'Landscape'),
+    )
     Categories = LabeledEnum('Categories', options)
     assert str(Categories.interior) == 'Categories.interior'
     assert Categories.interior.value == 'interior'
     assert Categories.interior.label == 'Interior'
+
+
+def test_labeled_enums_with_2_items(options):
+    Categories = LabeledEnum('Categories', options)
+    assert str(Categories.interior) == 'Categories.interior'
+    assert Categories.interior.value == 'interior'
+    assert Categories.interior.label == 'Interior'
+
+MCategories = LabeledEnum('MCategories', options())
+""" Pickable enums have to be retrievable from the module level."""
+
+
+def test_labeled_enums_are_pickable():
+    import pickle
+    assert pickle.dumps(MCategories.food)
+    v = pickle.dumps(MCategories.interior)
+    assert pickle.loads(v) is MCategories.interior
