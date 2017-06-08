bb1379029df57110e112c69df5f53eb941b71053
Ruda Porto Filgueiras <rudazz@gmail.com>
2017-06-08 13:06:03 +0200

    Finishing local roles test case with Order and Assignment model and local roles.

--- tests/db/mixins/test_local_roles.py
+++ tests/db/mixins/test_local_roles.py
@@ -15,6 +15,9 @@ CUSTOMER_ID_02 = uuid.uuid4()
 PROJECT_ID_01 = uuid.uuid4()
 PROJECT_ID_02 = uuid.uuid4()
 
+ORDER_ID_01 = uuid.uuid4()
+ORDER_ID_02 = uuid.uuid4()
+
 customer_data = [
     {
         'customer_managers': [
@@ -38,43 +41,63 @@ project_data = [
     {
         'parent_id': CUSTOMER_ID_01,
         'customer_pms': ['e9bee447-91ea-468f-b247-1ba4b9cf79ac'],
-        # 'customer_qas': ['3177f78e-6dee-44ce-b939-8135d1a8b777'],
-        # 'pms': ['e9bee447-91ea-468f-b247-1ba4b9cf79ac'],
-        # 'qas': ['92a40b92-8c04-407d-9922-097ba5171e2d'],
-        # 'scouts': ['edb4d4be-8b22-4818-894e-3da6317087f4'],
+        'customer_qas': ['3177f78e-6dee-44ce-b939-8135d1a8b777'],
+        'pms': ['e9bee447-91ea-468f-b247-1ba4b9cf79ac'],
+        'qas': ['92a40b92-8c04-407d-9922-097ba5171e2d'],
+        'scouts': ['edb4d4be-8b22-4818-894e-3da6317087f4'],
         'id': PROJECT_ID_01,
         'title': 'Project 01',
     },
     {
         'parent_id': CUSTOMER_ID_02,
         'customer_pms': ['e9bee447-91ea-468f-b247-1ba4b9cf79ac'],
-        # 'customer_qas': ['3177f78e-6dee-44ce-b939-8135d1a8b777'],
-        # 'pms': ['e9bee447-91ea-468f-b247-1ba4b9cf79ac'],
-        # 'qas': ['92a40b92-8c04-407d-9922-097ba5171e2d'],
-        # 'scouts': ['edb4d4be-8b22-4818-894e-3da6317087f4'],
+        'customer_qas': ['3177f78e-6dee-44ce-b939-8135d1a8b777'],
+        'pms': ['e9bee447-91ea-468f-b247-1ba4b9cf79ac'],
+        'qas': ['92a40b92-8c04-407d-9922-097ba5171e2d'],
+        'scouts': ['edb4d4be-8b22-4818-894e-3da6317087f4'],
         'id': PROJECT_ID_02,
         'title': 'Project 02',
     }
 ]
 
-order_data = {
-    'project_manager': ['5c78b972-e942-4bf8-a3d8-29b2417f1db2'],
-    'qa_manager': ['1b2d1c43-7609-4f73-9eea-d49ef79e215e'],
-    'scout_manager': ['64323e01-8662-46d0-a6e6-04d7013fa166'],
-    'id': '07ecfbe1-1518-43e6-8632-f36d3874678a',
-    'title': 'Order 01',
-}
+order_data = [
+    {
+        'parent_id': PROJECT_ID_01,
+        'customer_qa': ['5c78b972-e942-4bf8-a3d8-29b2417f1db2'],
+        'id': ORDER_ID_01,
+        'title': 'Order 01',
+    },
+    {
+        'parent_id': PROJECT_ID_02,
+        'customer_qa': ['425c19f8-0f59-4fb6-b345-491c243ff417'],
+        'id': ORDER_ID_02,
+        'title': 'Order 02',
+    }
 
-assignment_data = {
-    'project_manager': 'e9bee447-91ea-468f-b247-1ba4b9cf79ac',
-    'qa_manager': '92a40b92-8c04-407d-9922-097ba5171e2d',
-    'scout_manager': 'edb4d4be-8b22-4818-894e-3da6317087f4',
-    'id': '872a1092-50ac-4924-b78c-73a20bc4b592',
-    'title': 'Assignment 01',
-}
+]
+
+assignment_data = [
+    {
+        'parent_id': ORDER_ID_01,
+        'professional_user': ['e9bee447-91ea-468f-b247-1ba4b9cf79ac'],
+        'qa_manager': ['92a40b92-8c04-407d-9922-097ba5171e2d'],
+        'scout_manager': ['edb4d4be-8b22-4818-894e-3da6317087f4'],
+        'id': uuid.uuid4(),
+        'title': 'Assignment 01',
+    },
+    {
+        'parent_id': ORDER_ID_02,
+        'professional_user': ['e9bee447-91ea-468f-b247-1ba4b9cf79ac'],
+        'qa_manager': ['92a40b92-8c04-407d-9922-097ba5171e2d'],
+        'scout_manager': ['edb4d4be-8b22-4818-894e-3da6317087f4'],
+        'id': uuid.uuid4(),
+        'title': 'Assignment 02',
+    },
+
+]
 
 
-class Customer(BaseMetadata, SubItemMixin, Item):
+class Customer(SubItemMixin, BaseMetadata, Item):
     """Customer model."""
 
     __tablename__ = 'customers'
@@ -91,7 +114,7 @@ class Customer(BaseMetadata, SubItemMixin, Item):
 
     @customer_managers.setter
     def customer_managers(self, values: list):
-        """Update customer manager collection"""
+        """Update local role collection"""
         self.set_local_role(values, role_name='customer_managers')
 
     @customer_managers.expression
@@ -109,9 +132,9 @@ class Project(SubItemMixin, BaseMetadata, Item):
     __actors__ = (
         'customer_pms',
         'customer_qas',
-        # 'pms',
-        # 'qas',
-        # 'scouts',
+        'pms',
+        'qas',
+        'scouts',
     )
 
     @hybrid_property
@@ -121,7 +144,7 @@ class Project(SubItemMixin, BaseMetadata, Item):
 
     @customer_pms.setter
     def customer_pms(self, values: list):
-        """Update customer manager collection"""
+        """Update local role collection"""
         self.set_local_role(values, role_name='customer_pms')
 
     @customer_pms.expression
@@ -136,7 +159,7 @@ class Project(SubItemMixin, BaseMetadata, Item):
 
     @customer_qas.setter
     def customer_qas(self, values: list):
-        """Update customer manager collection"""
+        """Update local role collection"""
         self.set_local_role(values, role_name='customer_qas')
 
     @customer_qas.expression
@@ -144,26 +167,143 @@ class Project(SubItemMixin, BaseMetadata, Item):
         """Expression that return principal ids from database."""
         return cls.get_expression('customer_qas')
 
+    @hybrid_property
+    def scouts(self) -> list:
+        """Return the list of user_ids with local role of customer_manager."""
+        return self.principals_by_role('scouts')
 
-class Order(SubItemMixin, Item):
+    @scouts.setter
+    def scouts(self, values: list):
+        """Update local role collection"""
+        self.set_local_role(values, role_name='scouts')
+
+    @scouts.expression
+    def scouts(cls):
+        """Expression that return principal ids from database."""
+        return cls.get_expression('scouts')
+
+    @hybrid_property
+    def pms(self) -> list:
+        """Return the list of user_ids with local role of customer_manager."""
+        return self.principals_by_role('pms')
+
+    @pms.setter
+    def pms(self, values: list):
+        """Update local role collection"""
+        self.set_local_role(values, role_name='pms')
+
+    @pms.expression
+    def pms(cls):
+        """Expression that return principal ids from database."""
+        return cls.get_expression('pms')
+
+    @hybrid_property
+    def qas(self) -> list:
+        """Return the list of user_ids with local role of customer_manager."""
+        return self.principals_by_role('qas')
+
+    @qas.setter
+    def qas(self, values: list):
+        """Update local role collection"""
+        self.set_local_role(values, role_name='qas')
+
+    @qas.expression
+    def qas(cls):
+        """Expression that return principal ids from database."""
+        return cls.get_expression('qas')
+
+
+class Order(SubItemMixin, BaseMetadata, Item):
     """Order model."""
 
     __tablename__ = 'orders'
     __session__ = DBSession
 
+    __actors__ = (
+        'customer_qas',
+    )
 
-class Assignment(SubItemMixin, Item):
+    @hybrid_property
+    def customer_qa(self) -> list:
+        """Return the list of user_ids with local role of customer_manager."""
+        return self.principals_by_role('customer_qa')
+
+    @customer_qa.setter
+    def customer_qa(self, values: list):
+        """Update local role collection"""
+        self.set_local_role(values, role_name='customer_qa')
+
+    @customer_qa.expression
+    def customer_qa(cls):
+        """Expression that return principal ids from database."""
+        return cls.get_expression('customer_qa')
+
+
+class Assignment(SubItemMixin, BaseMetadata, Item):
     """Assignment model."""
 
     __tablename__ = 'assignments'
     __session__ = DBSession
 
+    __actors__ = (
+        'qa_manager',
+        'scout_manager',
+        'professional_user'
+    )
+
+    @hybrid_property
+    def qa_manager(self) -> list:
+        """Return the list of user_ids with local role of customer_manager."""
+        return self.principals_by_role('qa_manager')
+
+    @qa_manager.setter
+    def qa_manager(self, values: list):
+        """Update local role collection"""
+        self.set_local_role(values, role_name='qa_manager')
+
+    @qa_manager.expression
+    def qa_manager(cls):
+        """Expression that return principal ids from database."""
+        return cls.get_expression('qa_manager')
+
+    @hybrid_property
+    def scout_manager(self) -> list:
+        """Return the list of user_ids with local role of customer_manager."""
+        return self.principals_by_role('scout_manager')
+
+    @scout_manager.setter
+    def scout_manager(self, values: list):
+        """Update local role collection"""
+        self.set_local_role(values, role_name='scout_manager')
+
+    @scout_manager.expression
+    def scout_manager(cls):
+        """Expression that return principal ids from database."""
+        return cls.get_expression('scout_manager')
+
+    @hybrid_property
+    def professional_user(self) -> list:
+        """Return the list of user_ids with local role of customer_manager."""
+        return self.principals_by_role('professional_user')
+
+    @professional_user.setter
+    def professional_user(self, values: list):
+        """Update local role collection"""
+        self.set_local_role(values, role_name='professional_user')
+
+    @professional_user.expression
+    def professional_user(cls):
+        """Expression that return principal ids from database."""
+        return cls.get_expression('professional_user')
+
 
 model_tuples = (
     ('customer', Customer, customer_data, ('customer_managers', )),
-    ('project', Project, project_data, ('customer_pms', 'customer_qas', )),
-    # ('order', Order, order_data),
-    # ('assignment', Assignment, assignment_data),
+    ('project', Project, project_data,
+     ('customer_pms', 'customer_qas', 'pms', 'qas', 'scouts')),
+    ('order', Order, order_data, ('customer_qa',)),
+    ('assignment', Assignment, assignment_data,
+     ('qa_manager', 'scout_manager', 'professional_user')),
 )
 
 
