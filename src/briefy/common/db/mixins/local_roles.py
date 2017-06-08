452ee96cced8601f7edb7c3a8860fc4c5acfb19d
Ruda Porto Filgueiras <rudazz@gmail.com>
2017-06-08 15:52:17 +0200

    Refactor helper methods to be functions and clean the object name space.

--- src/briefy/common/db/mixins/local_roles.py
+++ src/briefy/common/db/mixins/local_roles.py
@@ -9,19 +9,73 @@ from sqlalchemy.orm.session import object_session
 import sqlalchemy as sa
 
 
+def get_lr_expression(cls, role_name):
+    """Get expression for a specific role name."""
+    from briefy.common.db.models.local_role import LocalRole
+    return sa.select([LocalRole.principal_id]).where(
+        sa.and_(
+            LocalRole.item_id == cls.id,
+            LocalRole.role_name == role_name
+        )
+    ).as_scalar()
+
+
+def principals_by_role(obj, role_name):
+    """Query principals with local roles in this Item."""
+    return [role.principal_id for role in obj.local_roles
+            if role.role_name == role_name]
+
+
+def set_local_role(obj, values: list, role_name: str):
+    """Set local role collection."""
+    from briefy.common.db.models.local_role import LocalRole
+    current_users = set(getattr(obj, role_name))
+    updated_users = set(values)
+    to_add = updated_users - current_users
+    to_remove = current_users - updated_users
+    session = object_session(obj)
+
+    if session and to_remove:
+        # delete
+        remove_roles = session.query(LocalRole).filter(
+            LocalRole.role_name == role_name,
+            LocalRole.principal_id.in_(to_remove),
+            LocalRole.item_id == obj.id
+        )
+        for lr in remove_roles:
+            logger.debug('Deleted: {0}'.format(lr))
+            session.delete(lr)
+
+        session.flush()
+
+    if session and to_add:
+        # add
+        for principal_id in to_add:
+            lr = LocalRole(
+                item_id=obj.id,
+                role_name=role_name,
+                principal_id=principal_id
+            )
+            session.add(lr)
+            obj.local_roles.append(lr)
+            logger.debug('Added: {0}'.format(lr))
+
+        session.flush()
+
+
 def make_lr_attr(actor):
     """Create local role hybrid_property attributes."""
     def getter(self):
         """Return the list of user_ids with a local role."""
-        return self.principals_by_role(actor)
+        return principals_by_role(self, actor)
 
     def setter(self, values):
         """Update local role collection."""
-        self.set_local_role(values, role_name=actor)
+        set_local_role(self, values, role_name=actor)
 
     def expression(cls):
         """Expression that return principal ids from database."""
-        return cls.get_lr_expression(actor)
+        return get_lr_expression(cls, actor)
 
     lr_attr = hybrid_property(getter)
     lr_attr.setter(setter)
@@ -51,55 +105,3 @@ class LocalRolesMixin:
             backref=sa.orm.backref('item'),
             lazy='noload'
         )
-
-    @classmethod
-    def get_lr_expression(cls, role_name):
-        """Get expression for a specific role name."""
-        from briefy.common.db.models.local_role import LocalRole
-        return sa.select([LocalRole.principal_id]).where(
-            sa.and_(
-                LocalRole.item_id == cls.id,
-                LocalRole.role_name == role_name
-            )
-        ).as_scalar()
-
-    def principals_by_role(self, role_name):
-        """Query principals with local roles in this Item."""
-        return [role.principal_id for role in self.local_roles
-                if role.role_name == role_name]
-
-    def set_local_role(self, values: list, role_name: str):
-        """Set local role collection."""
-        from briefy.common.db.models.local_role import LocalRole
-        current_users = set(getattr(self, role_name))
-        updated_users = set(values)
-        to_add = updated_users - current_users
-        to_remove = current_users - updated_users
-        session = object_session(self)
-
-        if session and to_remove:
-            # delete
-            remove_roles = session.query(LocalRole).filter(
-                LocalRole.role_name == role_name,
-                LocalRole.principal_id.in_(to_remove),
-                LocalRole.item_id == self.id
-            )
-            for lr in remove_roles:
-                logger.debug('Deleted: {0}'.format(lr))
-                session.delete(lr)
-
-            session.flush()
-
-        if session and to_add:
-            # add
-            for principal_id in to_add:
-                lr = LocalRole(
-                    item_id=self.id,
-                    role_name=role_name,
-                    principal_id=principal_id
-                )
-                session.add(lr)
-                self.local_roles.append(lr)
-                logger.debug('Added: {0}'.format(lr))
-
-            session.flush()
