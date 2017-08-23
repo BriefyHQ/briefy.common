f533cd0c1d162de4a208bee06b189ddb45382766
Ruda Porto Filgueiras <rudazz@gmail.com>
2017-08-23 18:08:52 +0200

    Adding typing hinting to local_roles mixin and fix tests.

--- src/briefy/common/db/mixins/local_roles.py
+++ src/briefy/common/db/mixins/local_roles.py
@@ -1,4 +1,5 @@
 """Local Roles mixin."""
+from briefy.common import db
 from briefy.common.db.comparator import BaseComparator
 from briefy.common.log import logger
 from sqlalchemy import and_
@@ -8,12 +9,20 @@ from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm.query import Query
 from sqlalchemy.orm.session import object_session
+from uuid import UUID
 
 import colander
 import sqlalchemy as sa
+import typing as t
 
 
-def get_lr_expression(cls, role_name):
+ItemType = 'db.models.Item'
+LocalRoleType = 'db.models.LocalRole'
+ClassItemType = t.ClassVar['db.models.Item']
+PrincipalIdType = t.TypeVar('PrincipalIdType', str, UUID)
+
+
+def get_lr_expression(cls: ClassItemType, role_name: str) -> t.Sequence[UUID]:
     """Get expression for a specific role name."""
     from briefy.common.db.models.local_role import LocalRole
 
@@ -25,13 +34,13 @@ def get_lr_expression(cls, role_name):
     ).as_scalar()
 
 
-def principals_by_role(obj, role_name):
+def principals_by_role(obj: ItemType, role_name: str) -> t.List[str]:
     """Query principals with local roles in this Item."""
     return [role.principal_id for role in obj.local_roles
             if role.role_name == role_name]
 
 
-def add_local_role(session, obj: object, role_name: str, principal_id: str):
+def add_local_role(session: db.Session, obj: object, role_name: str, principal_id: PrincipalIdType):
     """Add new local role."""
     from briefy.common.db.models.local_role import LocalRole
     payload = dict(
@@ -46,14 +55,18 @@ def add_local_role(session, obj: object, role_name: str, principal_id: str):
     logger.debug('Added: {0}'.format(lr))
 
 
-def del_local_role(session, obj: object, lr: object):
+def del_local_role(session: db.Session, obj: ItemType, lr: LocalRoleType):
     """Delete existing local role."""
     obj.local_roles.remove(lr)
     session.delete(lr)
     logger.debug('Deleted: {0}'.format(lr))
 
 
-def set_local_roles_by_role_name(obj: object, role_name: str, principal_ids: list):
+def set_local_roles_by_role_name(
+        obj: ItemType,
+        role_name: str,
+        principal_ids: t.List[PrincipalIdType]
+):
     """Set local role collection: for one role set all users."""
     current_users_list = getattr(obj, role_name)
     current_users = set(current_users_list)
@@ -77,7 +90,11 @@ def set_local_roles_by_role_name(obj: object, role_name: str, principal_ids: lis
         session.flush()
 
 
-def set_local_roles_by_principal(obj: object, principal_id: str, role_names: list):
+def set_local_roles_by_principal(
+        obj: ItemType,
+        principal_id: PrincipalIdType,
+        role_names: t.Sequence[str]
+):
     """Set local role collection: for one role set all users."""
     current_roles = {r.role_name for r in obj.local_roles
                      if str(r.principal_id) == str(principal_id)}
@@ -104,27 +121,27 @@ def set_local_roles_by_principal(obj: object, principal_id: str, role_names: lis
 class LocalRolesComparator(BaseComparator):
     """Comparator to filter by role_name and principal_id."""
 
-    def __init__(self, cls, role_name):
+    def __init__(self, cls: ClassItemType, role_name: str):
         """Initialize local roles comparator."""
         self.cls = cls
         self.role_name = role_name
 
-    def operate(self, op, other, escape=None):
+    def operate(self, op: t.Callable, other: t.Any, escape: t.Optional[str]=None) -> t.Callable:
         """Operate method return the transformation function."""
-        def transform(q):
+        def transform(query: Query):
             """Add a join condition with local_roles and add a filter by role_name in the query."""
             cls = self.cls.local_roles.mapper.class_
-            q = q.join(cls).filter(
+            query = query.join(cls).filter(
                 and_(
                     op(cls.principal_id, other),
                     cls.role_name == self.role_name
                 )
             )
-            return q
+            return query
         return transform
 
 
-def make_lr_attr(actor):
+def make_lr_attr(actor: str) -> hybrid_property:
     """Create local role hybrid_property attributes."""
     def getter(self):
         """Return the list of user_ids with a local role."""
@@ -134,11 +151,11 @@ def make_lr_attr(actor):
         """Update local role collection."""
         set_local_roles_by_role_name(self, actor, values)
 
-    def expression(cls):
+    def expression(cls: ClassItemType):
         """Expression that return principal ids from database."""
         return get_lr_expression(cls, actor)
 
-    def comparator(cls):
+    def comparator(cls: ClassItemType):
         """Return the custom local roles comparator."""
         return LocalRolesComparator(cls, actor)
 
@@ -171,7 +188,11 @@ class LocalRolesMixin:
         return set(cls.__actors__).union(cls.__additional_can_view_lr__)
 
     @classmethod
-    def query(cls, principal_id=None, permission='can_view') -> Query:
+    def query(
+            cls,
+            principal_id: t.Optional[PrincipalIdType]=None,
+            permission: str='can_view'
+    ) -> Query:
         """Return query object.
 
         :returns: A query object
